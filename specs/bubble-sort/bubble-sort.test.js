3df3cca49fa52871a438959b56571e1d3ec4a28c
Aanchal Kapoor <aanchal.kapoor878@gmail.com>
2021-06-04 12:19:08 +1000

    Bubble sort - my solution

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,60 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let outerIndex = 0;
+  let isSwappedAtLeastOnce = false;
+
+  do {
+    let isSwappedInThisIteration = resetFlag();
+
+    for (
+      let innerIndex = outerIndex + 1;
+      innerIndex < nums.length;
+      innerIndex++
+    ) {
+      if (nums[outerIndex] > nums[innerIndex]) {
+        nums = swap(outerIndex, innerIndex, nums);
+
+        outerIndex = incrementIndex(outerIndex);
+        isSwappedInThisIteration = setFlag();
+      } else {
+        outerIndex = incrementIndex(outerIndex);
+      }
+    }
+    outerIndex = resetIndex();
+    isSwappedAtLeastOnce = isSwappedInThisIteration;
+  } while (isSwappedAtLeastOnce);
+
+  return nums;
+}
+
+function swap(firstIndex, secondIndex, array) {
+  let temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+
+  return array;
+}
+
+function incrementIndex(index) {
+  return index + 1;
+}
+
+function resetIndex() {
+  return 0;
+}
+
+function setFlag() {
+  return true;
+}
+
+function resetFlag() {
+  return false;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
